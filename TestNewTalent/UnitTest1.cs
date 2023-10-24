53d6f1d323ac1bb373b9e73fd24b22ed56e08957
Napomuceno <erjeffersonnapomuceno@gmail.com>
2023-10-23 21:56:44 -0300

    first commit

---
+++ TestNewTalent/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NewTalents;
+using System;
+using Xunit;
+
+namespace TestNewTalent
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Calculadora Calc = new Calculadora();
+        }
+    }
+}
